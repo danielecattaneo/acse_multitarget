740d1ff8555dcebdfe360fd6e78f1327e639e1d6
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2020-01-19 22:54:33 +0100

    Add a new makefile for x86_64 tests

---
+++ acse/amd64/runtime/lance_rt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+extern void __lance_start(void) asm("__lance_start");
+extern int __axe_read(void) asm("__axe_read");
+extern void __axe_write(int n) asm("__axe_write");
+
+int main(int argc, char *argv[])
+{
+	__lance_start();
+}
+
+int __axe_read(void)
+{
+	int tmp;
+	scanf("%d", &tmp);
+	return tmp;
+}
+
+void __axe_write(int n)
+{
+	printf("%d\n", n);
+}
