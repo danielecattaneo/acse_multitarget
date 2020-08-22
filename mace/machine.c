5bd1d9030f754f0f906cb1b4f2a0b28585761bc1
Daniele Cattaneo <daniele.cattaneo@polimi.it>
2020-08-22 02:43:56 +0200

    Do a pass of clang-format over MACE.
    God it was SO UGLY

--- mace/machine.c
+++ mace/machine.c
@@ -1,10 +1,10 @@
 /*
  * Giovanni Agosta, Andrea Di Biagio
  * Politecnico di Milano, 2007
- * 
+ *
  * machine.c
  * Formal Languages & Compilers Machine, 2007/2008
- * 
+ *
  */
 
 #include "machine.h"
@@ -22,24 +22,24 @@ void print_psw(FILE *file)
       file = stderr;
 
    fprintf(file, "PSW = 0x%08x\n", psw);
-   fprintf(file,  "CARRY (C):\t %d\nOVERFLOW (V):\t %d\n"
-                  "ZERO (Z):\t %d\nNEGATIVE (N):\t %d\n\n"
-                  , getflag(CARRY), getflag(OVERFLOW)
-                  , getflag(ZERO), getflag(NEGATIVE));
+   fprintf(file,
+         "CARRY (C):\t %d\nOVERFLOW (V):\t %d\n"
+         "ZERO (Z):\t %d\nNEGATIVE (N):\t %d\n\n",
+         getflag(CARRY), getflag(OVERFLOW), getflag(ZERO), getflag(NEGATIVE));
 }
 
 /* Debug printf, print the content of the register file */
 void print_regs(FILE *file)
 {
-	int i;
+   int i;
 
    /* precondition */
    if (file == NULL)
       file = stderr;
-   
+
    fprintf(file, "\n*** REGISTER FILE STATUS ***\n");
-   for (i=0; i<NREGS; i++) {
-      fprintf(file, "R%-2d = 0x%08x%s", i, reg[i], i%4==3 ? "\n" : "  ");
+   for (i = 0; i < NREGS; i++) {
+      fprintf(file, "R%-2d = 0x%08x%s", i, reg[i], i % 4 == 3 ? "\n" : "  ");
    }
    if (i % 4 != 0)
       fprintf(file, "\n");
@@ -49,35 +49,36 @@ void print_regs(FILE *file)
 /* Debug, execute a memory dump      */
 void print_Memory_Dump(FILE *file, int lcode)
 {
-	int i;
+   int i;
 
    /* if not specified, `file' is set to stderr */
    if (file == NULL)
       file = stderr;
-   
-	fprintf(file,"*** Memory Dump *** \n");
-	for (i=0; i<lcode; i++) {
+
+   fprintf(file, "*** Memory Dump *** \n");
+   for (i = 0; i < lcode; i++) {
       if (i % 4 == 0)
          fprintf(file, "0x%08x:  ", i);
-	   fprintf(file,"0x%08x%s", mem[i], i%4==3 ? "\n" : "  ");
+      fprintf(file, "0x%08x%s", mem[i], i % 4 == 3 ? "\n" : "  ");
    }
    if (i % 4 != 0)
       fprintf(file, "\n");
-   
-	fprintf(file,"*** END Memory Dump *** \n");
+
+   fprintf(file, "*** END Memory Dump *** \n");
 }
 
 /* Get flag from processor status word */
 unsigned int getflag(int flag)
 {
-	return getbits(psw, flag, flag);
+   return getbits(psw, flag, flag);
 }
 
 /* Set flag in processor status word*/
-void setflag(int flag, int value){
-	int mask = 1<<flag;
-	if (value)
-		psw = psw | mask;
-	else 
-		psw = psw & ~mask;
+void setflag(int flag, int value)
+{
+   int mask = 1 << flag;
+   if (value)
+      psw = psw | mask;
+   else
+      psw = psw & ~mask;
 }
--- mace/machine.h
+++ mace/machine.h
@@ -1,10 +1,10 @@
 /*
  * Giovanni Agosta, Andrea Di Biagio
  * Politecnico di Milano, 2007
- * 
+ *
  * machine.h
  * Formal Languages & Compilers Machine, 2007/2008
- * 
+ *
  */
 #ifndef _MACHINE_H
 #define _MACHINE_H
@@ -21,13 +21,91 @@
 enum formats { TER, BIN, UNR, JMP };
 
 /* OPCODES (from 0 to 15) */
-enum opcTER { ADD, SUB, ANDL, ORL, EORL, ANDB, ORB, EORB, MUL, DIV, SHL, SHR, ROTL, ROTR, NEG, SPCL };
-enum opcBIN { ADDI, SUBI, ANDLI, ORLI, EORLI, ANDBI, ORBI, EORBI, MULI, DIVI, SHLI, SHRI, ROTLI, ROTRI, NOTL, NOTB };
-enum opcUNR { NOP, MOVA, JSR, RET, LOAD, STORE, HALT, SEQ, SGE, SGT, SLE, SLT, SNE, READ, WRITE, XPSW };
-enum opcJMP { BT, BF, BHI, BLS,	BCC, BCS, BNE, BEQ, BVC, BVS, BPL, BMI, BGE, BLT, BGT, BLE };
+enum opcTER {
+   ADD,
+   SUB,
+   ANDL,
+   ORL,
+   EORL,
+   ANDB,
+   ORB,
+   EORB,
+   MUL,
+   DIV,
+   SHL,
+   SHR,
+   ROTL,
+   ROTR,
+   NEG,
+   SPCL
+};
+enum opcBIN {
+   ADDI,
+   SUBI,
+   ANDLI,
+   ORLI,
+   EORLI,
+   ANDBI,
+   ORBI,
+   EORBI,
+   MULI,
+   DIVI,
+   SHLI,
+   SHRI,
+   ROTLI,
+   ROTRI,
+   NOTL,
+   NOTB
+};
+enum opcUNR {
+   NOP,
+   MOVA,
+   JSR,
+   RET,
+   LOAD,
+   STORE,
+   HALT,
+   SEQ,
+   SGE,
+   SGT,
+   SLE,
+   SLT,
+   SNE,
+   READ,
+   WRITE,
+   XPSW
+};
+enum opcJMP {
+   BT,
+   BF,
+   BHI,
+   BLS,
+   BCC,
+   BCS,
+   BNE,
+   BEQ,
+   BVC,
+   BVS,
+   BPL,
+   BMI,
+   BGE,
+   BLT,
+   BGT,
+   BLE
+};
 
 /* Exit codes (OK = no error) */
-enum term  { OK, NOARGS, NOFILE, INVALID_INSTR, INVALID_INSTR_FORMAT, MEM_FAULT, WRONG_FORMAT, WRONG_ARGS, BREAK };
+enum term {
+   OK,
+   NOARGS,
+   NOFILE,
+   INVALID_INSTR,
+   INVALID_INSTR_FORMAT,
+   MEM_FAULT,
+   WRONG_FORMAT,
+   WRONG_ARGS,
+   BREAK
+};
 
 /* Execution modes */
 enum modes { BASIC, SEGMENTED };
@@ -42,10 +120,10 @@ extern int mem[MEMSIZE];
 
 void print_Memory_Dump(FILE *file, int begin);
 
-extern unsigned int pc;  /* the program counter */
-extern int psw; /* the four condition flags */
+extern unsigned int pc; /* the program counter */
+extern int psw;         /* the four condition flags */
 
-enum flags { CARRY, OVERFLOW, ZERO, NEGATIVE } ;
+enum flags { CARRY, OVERFLOW, ZERO, NEGATIVE };
 
 /* Get flag from processor status word */
 unsigned int getflag(int flag);
